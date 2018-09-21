23303470221ff0a0a574e48d43f8a8b544b3711e
Dongming Zhang <dzhang123@gmail.com>
2018-09-21 14:51:58 -0500

    linq update

--- CSharpDynaCard/DynaCardApp.cs
+++ CSharpDynaCard/DynaCardApp.cs
@@ -15,7 +15,11 @@ namespace DynaCard
             if (args.Length < 1)
                 System.Console.WriteLine("Usage: DynaCardApp filename");
             List<(int, double, double weight)> values = (List<(int, double, double)>)(new DynaCardApp()).ParseFile(args[0]);
-            foreach ((int pos, double len, double weight) in values) {
+
+
+            List<(int pos, double len, double w)> first = FindStrokeCycles(values);
+
+            foreach ((int pos, double len, double weight) in first) {
                 System.Console.WriteLine($"{pos}, {len}, {weight}");
             }
 
@@ -24,8 +28,10 @@ namespace DynaCard
             System.Console.WriteLine();
 
 
+
+
             // test normalize
-            List<double> weights = (from w in values
+            List<double> weights = (from w in first
                                     select w.weight).ToList();
             List<double> nweights = Normalize(weights);
             foreach(var nw in nweights)
@@ -62,39 +68,56 @@ namespace DynaCard
         // TODO
         public static List<double> Normalize(List<double> values)
         {
-            
-            // do linq later
-            var min = Double.MaxValue;
-            var max = Double.MinValue;
-            foreach (var v in values) {
-                if (v < min)
-                    min = v;
-                if (v > max)
-                    max = v;
-            };
-            List<double> normalized = new List<double>();
-            var iter = values.GetEnumerator();
-            while (iter.MoveNext())
-            {
-                normalized.Add((iter.Current - min) / (max - min));
-            }
-            return normalized;
+            #region
+            //// do linq later
+            //var min = Double.MaxValue;
+            //var max = Double.MinValue;
+            //foreach (var v in values) {
+            //    if (v < min)
+            //        min = v;
+            //    if (v > max)
+            //        max = v;
+            //};
+            //List<double> normalized = new List<double>();
+            //var iter = values.GetEnumerator();
+            //while (iter.MoveNext())
+            //{
+            //    normalized.Add((iter.Current - min) / (max - min));
+            //}
+            //return normalized;
+            #endregion
+
+            double max = values.Max();
+            double min = values.Min();
+
+            IEnumerable<double> normalized =
+                                        from value in values
+                                        let normalized_value = (value - min) / (max - min)
+                                        select normalized_value;
+            return normalized.ToList();
         }
 
-        public List<(int position, double length, double weight)> FindFirstStrokeCycle(List<(int position, double length, double weight)> values)
+        // find stroke cycle either the first one or the entire "cycle"s, 
+        // in the latter case cycles will be averaged into a single cycle and return to the caller
+        public static List<(int position, double length, double weight)> 
+            FindStrokeCycles(List<(int position, double length, double weight)> values, bool allStrokeCycles = false)
         {
             // find indices of first pos=0 and second pos=0 to find the first cycle
-            // must use Linq or lambda
+            // 1. trim the values to eliminate incomplete cycles at both ends
+            IEnumerable<(int position, double, double)> cycles = values.SkipWhile(v => v.position != 0);
+            
+            cycles = cycles.Reverse().SkipWhile(v => v.position != 360).Reverse();
+            if (cycles.Count() == 0)
+                return cycles.ToList();
 
-
-            return null;
-        }
-
-        public List<(int position, double length, double weight)> FindAverageStrokeCycle(List<(int position, double length, double weight)> values)
-        {
-
-
-            return null;
+            List<int> positions = (from cycle in cycles select cycle.position).ToList();
+            
+            int cycleStartIndex = positions.IndexOf(0);
+            int cycleEndIndex = positions.IndexOf(360);
+            int count = cycleEndIndex - cycleStartIndex + 1;
+            List<(int, double, double)> singleCycle = cycles.Take(count).ToList();
+            // return first cycle for now.
+            return singleCycle;
         }
     }
 }
