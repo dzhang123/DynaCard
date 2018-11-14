688e0f4f2906a5b0a8f1b6d00c680f4c0c25f61f
Dongming Zhang <dzhang123@gmail.com>
2018-11-14 09:28:03 -0600

    some update for python code

--- PythonDynaCard/PythonDynaCard.py
+++ PythonDynaCard/PythonDynaCard.py
@@ -1,1 +1,24 @@
 
+
+import sys
+
+def parse_file (file):
+    filename = file
+    f = open(filename, 'rU')
+    points = []
+    for line in f:
+        if line.strip().startswith('#'):
+            continue
+        row = line.strip().split(',')
+        points.append((row[0], row[1], row[2]))
+
+    f.close()
+    return points
+
+def main():
+    if len(sys.argv[1]) > 1 :        
+        print (parse_file('full_pump.csv'))
+
+
+if __name__ == '__main__':
+    main()
---
+++ PythonDynaCard/foo.txt
@@ -0,0 +1,3 @@
+one line
+two line
+three line
